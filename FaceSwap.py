1d6c244c06e44f61e80de4b4f4edccd306e5c4ee
adityavaishampayan <aditya1.vaishampayan@gmail.com>
2020-02-26 18:28:37 -0500

    add bradley coopers face

--- FaceSwap.py
+++ FaceSwap.py
@@ -14,7 +14,7 @@ detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
 
-img = cv2.imread("aditya.jpg")
+img = cv2.imread("bradley_cooper.jpg")
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 mask = np.zeros_like(gray)
 
@@ -33,26 +33,50 @@ for face in faces:
         x = landmarks.part(n).x
         y = landmarks.part(n).y
         landmark_points.append((x,y))
-        cv2.circle(img,(x,y),3,(0,0,255),-1)
+        cv2.circle(img,(x,y),3,(0,0,255),1)
         cv2.imshow("facial landmarks",img)
-        cv2.imwrite('facial_landmarks.png',img)
+        cv2.imwrite('facial_landmarks_1.png',img)
 
     points = np.array(landmark_points,np.int32)
 
     convexhull = cv2.convexHull(points)
 
-    cv2.polylines(img,[convexhull],True,(0,255,0),2)
+    cv2.polylines(img,[convexhull],True,(255,0,0),2)
 
     cv2.fillConvexPoly(mask,convexhull,255)
 
 
     face_image_1 = cv2.bitwise_and(img,img,mask = mask)
 
+    # Delaunay Triangulation
+
+    rect = cv2.boundingRect(convexhull)
+    subdiv = cv2.Subdiv2D(rect)
+    subdiv.insert(landmark_points)
+
+    triangles = subdiv.getTriangleList()
+    triangles = np.array(triangles,dtype = np.int32)
+
+    for t in triangles:
+        pt1 = (t[0],t[1])
+        pt2 = (t[2],t[3])
+        pt3 = (t[4],t[5])
+
+        cv2.line(img,pt1,pt2,(0,255,0),1)
+        cv2.line(img,pt2,pt3,(0,255,0),1)
+        cv2.line(img,pt3,pt1,(0,255,0),1)
+
+
+
+
+
+
+
 cv2.imshow("frame",img)
-cv2.imwrite('frame.png',img)
+cv2.imwrite('frame_1.png',img)
 
 cv2.imshow("face_image",face_image_1)
-cv2.imwrite('face_image.png',face_image_1)
+cv2.imwrite('face_image_1.png',face_image_1)
 
 
 cv2.waitKey(0)
